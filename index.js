c435f8fd3cb7db44bc34220fb2aa200ab385e836
SarthakDandotiya <sarthak.dandotiya23@gmail.com>
2019-07-16 10:10:16 +0530

    Templates and View Engine

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
-const router = express.Router();
+const exphbs = require('express-handlebars');
 
 const logger = require('./middleware/logger');
+const members = require('./Members');
 
 const app = express();
 
@@ -11,9 +12,21 @@ const PORT = process.env.PORT || 5000;
 // Init Middleware
 app.use(logger);
 
+// Handlebars middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
 // Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Homepage route
+app.get('/', (req, res) => {
+	res.render('index', {
+		title: 'Members App',
+		members
+	});
+});
 
 // Set a static folder
 app.use(express.static(path.join(__dirname, 'public')));
--- routes/api/members.js
+++ routes/api/members.js
@@ -42,6 +42,8 @@ router.post('/', (req, res) => {
 
 	members.push(newMember);
 	res.json(members);
+
+	// res.redirect('/');
 });
 
 // Update Member
